5056ad429e9a0221981866ba1cba930821e5a4fb
MirAfzalul <mirafzalulbashar@gmail.com>
2023-10-01 17:08:03 -0400

    v2.0 now user can chose length of password between 8,12 and 16

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const passwordBox =document.getElementById("password");
-const length=12;
+const length=8;
 const upperCase="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 const lowerCase="abcdefghijklmnopqrstuvwxyz";
 const number="0123456789"
 const symbol="~`!@#$%^&*()_+{}[]|\:;',./<>?";
 const allChars=upperCase+lowerCase+number+symbol; 
+const l2=12;
+const l3=16;
 console.log("flag")
-function createPassword(){
+function createPasswordLength8(){
     let password="";
     password+=upperCase[Math.floor(Math.random()*upperCase.length)];
     password+=lowerCase[Math.floor(Math.random()*lowerCase.length)];
@@ -20,7 +22,37 @@ function createPassword(){
 
     
 }
+function createPasswordL2(){
+    let password="";
+    password+=upperCase[Math.floor(Math.random()*upperCase.length)];
+    password+=lowerCase[Math.floor(Math.random()*lowerCase.length)];
+    password+=number[Math.floor(Math.random()*number.length)];
+    password+=symbol[Math.floor(Math.random()*symbol.length)];
+
+    while(l2>password.length){
+        password+=allChars[Math.floor(Math.random()*allChars.length)];
+    }
+    passwordBox.value=password;
+
+    
+}
+function createPasswordL3(){
+    let password="";
+    password+=upperCase[Math.floor(Math.random()*upperCase.length)];
+    password+=lowerCase[Math.floor(Math.random()*lowerCase.length)];
+    password+=number[Math.floor(Math.random()*number.length)];
+    password+=symbol[Math.floor(Math.random()*symbol.length)];
+
+    while(l3>password.length){
+        password+=allChars[Math.floor(Math.random()*allChars.length)];
+    }
+    passwordBox.value=password;
+
+    
+}
 function copyPassword(){
     passwordBox.select();
-    document.execCommand("copy");
+    //document.execCommand("copy");
+    navigator.clipboard.writeText(passwordBox.value);
+    //alert("Copied the text: " + passwordBox.value);
 }
