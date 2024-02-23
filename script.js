e7b5ada656d101a04cc421ca6f1891f5b9cf592f
MirAfzalul <mirafzalulbashar@gmail.com>
2024-02-22 20:58:44 -0500

    password length change is dynamic now

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const passwordBox =document.getElementById("password");
-const length=8;
+// const length=8;
 const upperCase="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 const lowerCase="abcdefghijklmnopqrstuvwxyz";
 const number="0123456789"
@@ -8,48 +8,29 @@ const allChars=upperCase+lowerCase+number+symbol;
 const l2=12;
 const l3=16;
 console.log("flag")
+
+    
+
+
 function createPasswordLength8(){
     let password="";
+   
+    l=document.getElementById("lengthInput").value;
+    console.log(l)
     password+=upperCase[Math.floor(Math.random()*upperCase.length)];
     password+=lowerCase[Math.floor(Math.random()*lowerCase.length)];
     password+=number[Math.floor(Math.random()*number.length)];
     password+=symbol[Math.floor(Math.random()*symbol.length)];
 
-    while(length>password.length){
+    while(l>password.length){
         password+=allChars[Math.floor(Math.random()*allChars.length)];
     }
     passwordBox.value=password;
 
     
 }
-function createPasswordL2(){
-    let password="";
-    password+=upperCase[Math.floor(Math.random()*upperCase.length)];
-    password+=lowerCase[Math.floor(Math.random()*lowerCase.length)];
-    password+=number[Math.floor(Math.random()*number.length)];
-    password+=symbol[Math.floor(Math.random()*symbol.length)];
 
-    while(l2>password.length){
-        password+=allChars[Math.floor(Math.random()*allChars.length)];
-    }
-    passwordBox.value=password;
 
-    
-}
-function createPasswordL3(){
-    let password="";
-    password+=upperCase[Math.floor(Math.random()*upperCase.length)];
-    password+=lowerCase[Math.floor(Math.random()*lowerCase.length)];
-    password+=number[Math.floor(Math.random()*number.length)];
-    password+=symbol[Math.floor(Math.random()*symbol.length)];
-
-    while(l3>password.length){
-        password+=allChars[Math.floor(Math.random()*allChars.length)];
-    }
-    passwordBox.value=password;
-
-    
-}
 function copyPassword(){
     passwordBox.select();
     //document.execCommand("copy");
